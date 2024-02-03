c972303fd7afea6da5e612b301d9b55d9159ccef
Crinax <agreedeth@gmail.com>
2024-02-03 23:27:53 +0300

    fix(api/products): fix invalid route matcher initialization

--- src/api/v1/products/get_products.rs
+++ src/api/v1/products/get_products.rs
@@ -12,12 +12,12 @@ pub(super) async fn get_products(products_service: Data<ProductService>) -> impl
     products.map(|data| HttpResponse::Ok().json(data)).unwrap()
 }
 
-#[get(":id")]
+#[get("{id}")]
 pub(super) async fn get_concreate_product(
-    path_id: Path<u32>,
+    id: Path<u32>,
     product_service: Data<ProductService>
 ) -> impl Responder {
-    let product = product_service.get(path_id.into_inner()).await
+    let product = product_service.get(id.into_inner()).await
         .map(|data| HttpResponse::Ok().json(data))
         .map_err(|err| match err {
             ProductServiceErr::Internal => ApiError::internal_error(),
